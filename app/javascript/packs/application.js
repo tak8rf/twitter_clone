e539e73bc2f08c1286415e63f9ed3401ed99ad9e
Takumi Nagashima <emperte2008@gmail.com>
2022-07-25 19:37:54 +0900

    edit to update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../stylesheets/application';
+import '../stylesheets/sample';
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
